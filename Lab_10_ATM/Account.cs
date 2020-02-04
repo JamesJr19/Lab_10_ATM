c250570d964c79bf86c5cff26a9075b700d1c7e0
jamesjr19 <ruwartja@gmail.com>
2020-02-03 21:15:50 -0500

    Lab_10_ATM Push 1

---
+++ Lab_10_ATM/Account.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_10_ATM
+{
+    class Account
+    {
+        private string name;
+        private string password;
+        private int balance;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public int Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+
+
+        public Account()
+        {
+
+        }
+
+        public Account(string _name, string _password, int _balance)
+        {
+            name = _name;
+            password = _password;
+            balance = _balance;
+        }
+
+    }
+}
+//#1 - Register Account. Takes a username and password and uses them to create a new account, and add to list of active accounts
+
+        //#2 - Login. Takes username and password.
+            // first, check if an account is logged in. If so, kick an error (Current user must log out). 
+            // If not, search the account list for an Account with a matching username AND password 
+            // once found, set that account to the Current Account
+
+
+        // ONLY IF THERE IS AN ACCOUNT LOGGED IN
+        //#3. Logout - Set the current account to null
+
+        //#4. CheckBalance - Prints the current Account Balance (double)
+
+        //#5. Deposit - Takes a double and adds it to the current AccountBalance
+
+        //#6. Withdraw - Takes a double and subtracts from AccountBalance
+            // if AccountBalance < ammountToWithdraw - throw an error.
+
+
+    }
+}
---
+++ Lab_10_ATM/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab_10_ATM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Grand Circus ATM");
+            Console.WriteLine("This will probably be all I get done tonight");
+
+
+
+        }
+    }
+}
